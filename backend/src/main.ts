1aadda3486260d4ea60ec11325966d917a251b3e
Priya Chakraborty <priya@yourflow.com.au>
2024-09-06 01:18:21 +0530

    Commit: Backend

---
+++ backend/src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  const baseUrl = configService.getOrThrow('BASEURL');
+  // swagger openapi
+  const swaggerPrefix = 'swagger';
+  const config = new DocumentBuilder()
+    .setTitle('Book Store Api')
+    .setDescription('The books API description')
+    .setVersion('v1.0')
+    .addServer(baseUrl)
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(swaggerPrefix, app, document);
+  await app.listen(port);
+  Logger.log(`🚀 Application is running on: ${baseUrl}`);
+  Logger.log(`🚀 Swagger is running on: ${baseUrl}/${swaggerPrefix}`)
+}
+bootstrap();
